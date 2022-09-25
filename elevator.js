c474048953c13b6532995a09b41ef49de94f648f
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2022-09-25 08:21:20 -0400

    18 extended javascript version to multiple elevators

--- elevator.js
+++ elevator.js
@@ -1,152 +1,184 @@
-   var timer = null;
-   var timer2 = null;
-   const timing = 30;
-   var is_open = new Boolean;
-   var globalcontent = "";
-   let width = 0;
-   let height = 0;
-   let padtop = 0;
-   let half = 0;
-   let x = 0;
-   let y = 0;
-   let border = 1;
-   var lock = undefined;
-   onload = () => {
-      var elevator = document.getElementById("elevator");
-      var content = elevator.innerHTML;
-      elevator.innerHTML = "<div class='door' id='left_door'></div><p id='textcontent'>" + content + "</p><div class='door' id='right_door'></div>";
-      text = document.getElementById("textcontent");
-      globalcontent = text.innerHTML;
-      if(document.getElementById("elevator").getAttribute("height") && document.getElementById("elevator").getAttribute("width")){
-         height = document.getElementById("elevator").getAttribute("height");
-         width = document.getElementById("elevator").getAttribute("width");
-      } else {
-         height = 300;
-         width = 300;
-      }
-      document.getElementById("elevator").style.width = width + "px";
-      document.getElementById("elevator").style.height = height + "px";
-      half = Number(width) / 2;
-      x = 0;
-      y = 0;
-      document.getElementById("left_door").style.width = half + "px";
-      document.getElementById("right_door").style.width = (half - border) + "px";
-      document.getElementById("left_door").style.height = height + "px";
-      document.getElementById("right_door").style.height = height + "px";
-      document.getElementById("left_door").style.border = border + "px solid black";
-      document.getElementById("right_door").style.border = border + "px solid black";
-      if(document.getElementById("elevator").getAttribute("image")){
-         left_door = document.getElementById("left_door");
-         right_door = document.getElementById("right_door");
-         left_image = document.createElement('img');
-         left_image.src = document.getElementById("elevator").getAttribute("image");
-         left_image.width = width;
-         left_image.height = height;
-         left_image.style.clipPath = "inset(0% 50% 0% 0%)";
-         left_image.style.webkitClipPath = "inset(0% 50% 0% 0%)";
-         left_door.style.overflow = "hidden";
-         left_door.appendChild(left_image);
-         right_image = document.createElement('img');
-         right_image.src = document.getElementById("elevator").getAttribute("image");
-         right_image.width = width;
-         right_image.height = height;
-         right_image.style.clipPath = "inset(0% 0% 0% 50%)";
-         right_image.style.webkitClipPath = "inset(0% 0% 0% 50%)";
-         right_door.style.overflow = "hidden";
-         right_door.appendChild(right_image);
-         right_door.firstChild.style.position = "relative";
-         right_door.firstChild.style.left = "-" + half + "px";
-      }
-      is_open = true;
-      lock = 1;
-      timer = setInterval(lowertext,1);
-      document.getElementById("elevator").onclick = () => {
-         if(typeof(lock)=="undefined"){
-           lock = 0;
-           left_door = document.getElementById("left_door");
-           right_door = document.getElementById("right_door");
-           is_open = !is_open;
-           if(is_open){
-              timer = setInterval(lowertext,timing);
-              timer2 = setInterval(closedoors,timing);
-           } else {
-              timer = setInterval(opendoors,timing);
-              timer2 = setInterval(raisetext,timing);
-           }
-         }
-      }
-   }
-   opendoors = () => {
-         console.log("opening doors " + x + " " + y);
-         left_door = document.getElementById("left_door");
-         right_door = document.getElementById("right_door");
-         x += 10;
-         y += 10;
-         if(x > 100){
-            ++lock;
-            x = 100;
-            y = 100;
-            clearInterval(timer);
-            if(lock >= 2){
-              lock = undefined;
-            }
-            return;
-         }
-         left_door.style.clipPath = "inset(0% " + x + "% 0% 0%)";
-         left_door.style.webkitClipPath = "inset(0% " + x + "% 0% 0%)";
-         right_door.style.clipPath = "inset(0% 0% 0% " + y + "%)";
-         right_door.style.webkitClipPath = "inset(0% 0% 0% " + y + "%)";
-   }
-   raisetext = () => {
-        console.log("raising text height " + height + " padding " + padtop);
-        padtop -= 10;
-        if(padtop <= 0){
-           ++lock;
-           padtop = 0;
-           clearInterval(timer2);
-           document.getElementById("elevator").style.overflow = "scroll";
-           if(lock >= 2){
-             lock = undefined;
-           }
-           return;
-        }
-        document.getElementById("textcontent").style.paddingTop = padtop + "px";
-   }
-   lowertext = () => {
-        console.log("lowering text height " + height + " padding " + padtop);
-        padtop += 10;
-        if(padtop > height){
-           ++lock;
-           clearInterval(timer);
-           document.getElementById("elevator").style.overflow = "hidden";
-           if(lock >= 2){
-             lock = undefined;
-             document.getElementById("elevator").scrollTop = "0px";
-           }
-           return;
-        }
-        document.getElementById("textcontent").style.paddingTop = padtop + "px";
-   }
-   closedoors = () => {
-         console.log("closing doors " + x + " " + y);
-         document.getElementById("elevator").scrollTop = "0px";
-         x -= 10;
-         y -= 10;
-         if(x < 0){
-            ++lock;
-            x = 0;
-            y = 0;
-            clearInterval(timer2);
-            if(lock >= 2){
-              lock = undefined;
-              document.getElementById("elevator").scrollTop = "0px";
-            }
-            return;
-         }
-         left_door = document.getElementById("left_door");
-         right_door = document.getElementById("right_door");
-         left_door.style.clipPath = "inset(0% " + x + "% 0% 0%)";
-         left_door.style.webkitClipPath = "inset(0% " + x + "% 0% 0%)";
-         right_door.style.clipPath = "inset(0% 0% 0% " + y + "%)";
-         right_door.style.webkitClipPath = "inset(0% 0% 0% " + y + "%)";
-   }
+let Elevators = [];
+class Elevator {
+	constructor(k,height,width,mask){
+		this.is_open = true;
+		this.mask = mask;
+		this.height = height;
+		this.width = width;
+		this.padtop = 0;
+		this.x = 0;
+		this.y = 0;
+		this.half = 0;
+		this.timing = 30;
+		this.index = k;
+		this.border = 1;
+		this.lock = 1;
+		document.getElementsByClassName("elevator")[this.index].onclick = () => {
+			var elevator = Elevators[this.index];
+			if(typeof(elevator.lock)=="undefined"){
+				elevator.lock = 0;
+				left_door = document.querySelectorAll(".elevator-left-door")[k];
+				right_door = document.querySelectorAll(".elevator-right-door")[k];
+				elevator.is_open = !elevator.is_open;
+				if(elevator.is_open){
+					elevator.timer = setInterval(function(){elevator.lowertext()}.bind(elevator),elevator.timing);
+					elevator.timer2 = setInterval(function(){elevator.closedoors()}.bind(elevator),elevator.timing);
+				} else {
+					elevator.timer = setInterval(function(){elevator.opendoors()}.bind(elevator),elevator.timing);
+					elevator.timer2 = setInterval(function(){elevator.raisetext()}.bind(elevator),elevator.timing);
+				}
+			}
+		}
+
+		this.timer = null;
+		this.timer2 = null;
+		this.timer = setInterval(function(){this.lowertext()}.bind(this),1);
+	}
+	show_mask(){
+		if(this.mask){
+			var mask = document.getElementById("elevator" + this.index).getElementsByClassName("elevator-overlay")[0].firstChild;
+			mask.style.visibility = "visible";
+		}
+	}
+	hide_mask(){
+		if(this.mask){
+			var mask = document.getElementById("elevator" + this.index).getElementsByClassName("elevator-overlay")[0].firstChild;
+			mask.style.visibility = "hidden";
+		}
+	}
+	opendoors(){
+		console.log("opening doors " + this.x + " " + this.y);
+		this.hide_mask();
+		left_door = document.querySelectorAll(".elevator-left-door")[this.index];
+		right_door = document.querySelectorAll(".elevator-right-door")[this.index];
+		this.x += 10;
+		this.y += 10;
+		if(this.x > 100){
+			++this.lock;
+			this.x = 100;
+			this.y = 100;
+			clearInterval(this.timer);
+			if(this.lock >= 2){
+				this.lock = undefined;
+			}
+			return;
+		}
+		left_door.style.clipPath = "inset(0% " + this.x + "% 0% 0%)";
+		left_door.style.webkitClipPath = "inset(0% " + this.x + "% 0% 0%)";
+		right_door.style.clipPath = "inset(0% 0% 0% " + this.y + "%)";
+		right_door.style.webkitClipPath = "inset(0% 0% 0% " + this.y + "%)";
+	}
+	raisetext(){
+		console.log("raising text height " + this.height + " padding " + this.padtop);
+		this.padtop -= 10;
+		if(this.padtop <= 0){
+			++this.lock;
+			this.padtop = 0;
+			clearInterval(this.timer2);
+			document.getElementsByClassName("elevator")[this.index].style.overflow = "scroll";
+			if(this.lock >= 2){
+				this.lock = undefined;
+			}
+			return;
+		}
+		document.getElementsByClassName("elevator-text-content")[this.index].style.paddingTop = this.padtop + "px";
+	}
+	lowertext(){
+		console.log("index " + this.index + " lowering text height " + this.height + " padding " + this.padtop);
+		this.padtop += 10;
+		if(this.padtop > this.height){
+			++this.lock;
+			clearInterval(this.timer);
+			document.getElementsByClassName("elevator")[this.index].style.overflow = "hidden";
+			if(this.lock >= 2){
+				this.lock = undefined;
+				document.getElementsByClassName("elevator")[this.index].scrollTop = "0px";
+			}
+			return;
+		}
+		document.getElementsByClassName("elevator-text-content")[this.index].style.paddingTop = this.padtop + "px";
+	}
+	closedoors(){
+		console.log("closing doors " + this.x + " " + this.y);
+		document.getElementsByClassName("elevator")[this.index].scrollTop = "0px";
+		this.x -= 10;
+		this.y -= 10;
+		if(this.x < 0){
+			++this.lock;
+			this.x = 0;
+			this.y = 0;
+			clearInterval(this.timer2);
+			if(this.lock >= 2){
+				this.lock = undefined;
+				document.getElementsByClassName("elevator")[this.index].scrollTop = "0px";
+			}
+			this.show_mask();
+			return;
+		}
+		left_door = document.querySelectorAll(".elevator-left-door")[this.index];
+		right_door = document.querySelectorAll(".elevator-right-door")[this.index];
+		left_door.style.clipPath = "inset(0% " + this.x + "% 0% 0%)";
+		left_door.style.webkitClipPath = "inset(0% " + this.x + "% 0% 0%)";
+		right_door.style.clipPath = "inset(0% 0% 0% " + this.y + "%)";
+		right_door.style.webkitClipPath = "inset(0% 0% 0% " + this.y + "%)";
+	}
+}
+onload = () => {
+	var elevators = document.getElementsByClassName("elevator");
+	for(let k = 0; k < elevators.length; k++){
+		var elevator = elevators[k];
+		elevator.id = "elevator" + k;
+		if(elevator.getAttribute("height") && elevator.getAttribute("width")){
+			var height = elevator.getAttribute("height");
+			var width = elevator.getAttribute("width");
+		} else {
+			var height = 300;
+			var width = 300;
+		}
+		elevator.style.width = width + "px";
+		elevator.style.height = height + "px";
+		var mask = false;
+		if(elevator.getAttribute("mask") && elevator.getAttribute("image")){
+			mask = true;
+		}
+		var content = elevator.innerHTML;
+		if(mask){
+			elevator.innerHTML = "<div class='elevator-door elevator-left-door'></div><p class='elevator-text-content'></p><div class='elevator-door elevator-right-door'></div><div class='elevator-overlay'><img src='" + elevator.getAttribute("image") + "' style='position:relative;z-index:10;width:" + width + "px;height:" + height + "px;'></div>";
+		} else {
+			elevator.innerHTML = "<div class='elevator-door elevator-left-door'></div><p class='elevator-text-content'></p><div class='elevator-door elevator-right-door'></div>";
+		}
+		var text = document.getElementsByClassName("elevator-text-content")[k];
+		text.innerHTML = content;
+		let half = Number(width) / 2;
+		let border = 1;//move into class
+		left_door = document.querySelectorAll(".elevator-left-door")[k];
+		right_door = document.querySelectorAll(".elevator-right-door")[k];
+		left_door.style.width = half + "px";
+		right_door.style.width = (half - border) + "px";
+		left_door.style.height = height + "px";
+		right_door.style.height = height + "px";
+		left_door.style.border = border + "px solid black";
+		right_door.style.border = border + "px solid black";
+		if(elevator.getAttribute("image")){
+			left_image = document.createElement('img');
+			left_image.src = elevator.getAttribute("image");
+			left_image.width = width;
+			left_image.height = height;
+			left_image.style.clipPath = "inset(0% 50% 0% 0%)";
+			left_image.style.webkitClipPath = "inset(0% 50% 0% 0%)";
+			left_door.style.overflow = "hidden";
+			left_door.appendChild(left_image);
+			right_image = document.createElement('img');
+			right_image.src = elevator.getAttribute("image");
+			right_image.width = width;
+			right_image.height = height;
+			right_image.style.clipPath = "inset(0% 0% 0% 50%)";
+			right_image.style.webkitClipPath = "inset(0% 0% 0% 50%)";
+			right_door.style.overflow = "hidden";
+			right_door.appendChild(right_image);
+			right_door.firstChild.style.position = "relative";
+			right_door.firstChild.style.left = "-" + half + "px";
+		}
+		Elevators.push(new Elevator(k,height,width,mask));
+	}
+}
